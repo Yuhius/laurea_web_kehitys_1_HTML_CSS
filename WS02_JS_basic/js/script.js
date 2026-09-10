8c5613f57355817018da92a45396f36161a7f773
Juha <Juha.Korhonen@student.laurea.fi>
2026-09-10 14:39:37 +0300

    Tehty muutoksia

---
+++ WS02_JS_basic/js/script.js
@@ -0,0 +1,19 @@
+console.log("Hello, World!");
+
+const userName = "Juha";
+const favoriteAnimal = "Mäyräkoira";
+
+console.log("My name is " + userName);
+console.log("My favorite animal is " + favoriteAnimal);
+
+alert("Tervetuloa hienoille nettisivuilleni!")
+
+const visitorName = prompt("Mikä on nimesi?");
+
+console.log(visitorName);
+
+console.log("Terve, " + visitorName + "! Tervetuloa javascriptin pariin!");
+
+const visitorFavoriteAnimal = prompt("Mikä on lempieläimesi?");
+
+console.log("Terve, " + visitorName + "! Lempieläimesi on " + visitorFavoriteAnimal + ".");
