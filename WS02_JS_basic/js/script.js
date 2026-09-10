931998ca9dd627aa5a54cb4823de6a1a896649aa
Juha <Juha.Korhonen@student.laurea.fi>
2026-09-10 15:04:36 +0300

    Tehty lisää muutoksia

--- WS02_JS_basic/js/script.js
+++ WS02_JS_basic/js/script.js
@@ -17,3 +17,19 @@ console.log("Terve, " + visitorName + "! Tervetuloa javascriptin pariin!");
 const visitorFavoriteAnimal = prompt("Mikä on lempieläimesi?");
 
 console.log("Terve, " + visitorName + "! Lempieläimesi on " + visitorFavoriteAnimal + ".");
+
+const visitorAge = prompt("Mikä on ikäsi?");
+
+if (visitorAge >= 18) {
+    result = "Sinä olet aikuinen!";
+} else {
+    result = "Sinä olet lapsi!";
+}
+
+console.log(result);
+
+function greetUser(name) {
+    console.log("Terve " + name + "!");
+}
+
+greetUser("Juha");
