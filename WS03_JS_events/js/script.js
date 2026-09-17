291334d41921933c75a0837f314b617f87f59abf
Juha <Juha.Korhonen@student.laurea.fi>
2026-09-17 15:00:24 +0300

    Tehty lisää tehtäviä

--- WS03_JS_events/js/script.js
+++ WS03_JS_events/js/script.js
@@ -1,7 +1,35 @@
-function showMessage() {
-    alert("You clicked me!");
-}
+const showMessage = document.querySelector("#showMessage")
 
-function showTable() {
-    
-}
+showMessage.addEventListener("click", function (){
+    alert("You clicked me!");
+});
+
+const showTable = document.querySelector("#showTable")
+
+showTable.addEventListener("click", function (){
+    const table = `
+    <table border="1">
+        <thead>
+            <tr>
+                <th>Animal</th>
+                <th>Habitat</th>
+                <th>Diet</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Tiger</td>
+                <td>Forest</td>
+                <td>Carnivore</td>
+            </tr>
+            <tr>
+                <td>Elephant</td>
+                <td>Savanna</td>
+                <td>Herbivore</td>
+            </tr>
+        </tbody>
+    </table>
+`;
+
+document.getElementById("tableContainer").innerHTML = table;
+});
